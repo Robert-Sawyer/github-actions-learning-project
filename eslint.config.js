cf3745642941956bfd7f9797d845cdb6aa29412e
Robert Olbrychowski <47324286+Robert-Sawyer@users.noreply.github.com>
2026-07-12 13:20:39 +0200

    Fix bug with workflow and resolve conflicts in deps

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+
+export default [
+  {
+    files: ['src/**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      react,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      'react/react-in-jsx-scope': 'off',
+      semi: [2, 'always'],
+    },
+  },
+];
